db500605464fd7c89b1227d8a4c29bff028d7386
kazz <gang9696@gmail.com>
2023-10-04 23:28:23 +0900

    GDI plus

--- winmain.cpp
+++ winmain.cpp
@@ -76,12 +76,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance,
 	return msg.wParam;
 }
 
+void OnPaint(HWND hwnd)
+{
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hwnd, &ps);
+
+	HPEN redPen = CreatePen(PS_SOLID, 1, RGB(255,0,0));
+	HBRUSH hatchBrush = CreateHatchBrush(HS_CROSS, RGB(255,0,0));
+
+	SelectObject(hdc, redPen);
+	SelectObject(hdc, hatchBrush);
+	Rectangle(hdc, 0, 0, 100, 100); // 사각형 그리기
+
+	DeleteObject(hatchBrush);
+	DeleteObject(redPen);
+	EndPaint(hwnd, &ps);
+}
+
 // 4. '윈도우프로시저' 작성
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
-
+	case WM_PAINT: // 윈도우가 갱신돼도 다시 그림. GDI의 기본 방식: 그릴건 모아서 그린다
+	{
+		OnPaint(hWnd);
+		break;
+	}
 	case WM_KEYDOWN:
 	{
 		std::ostringstream oss;
@@ -92,7 +113,13 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 	case WM_LBUTTONDOWN:
 	{
 
-
+		/* 정적인 방식
+		HDC hdc;
+		
+		hdc = GetDC(hWnd); // 현재 윈도우의 그리기 영역을 가져옴(포인터임)
+		Rectangle(hdc, 0, 0, 100, 100); // 사각형 그리기
+		ReleaseDC(hWnd, hdc); // 포인터이기에 겟DC를 하면 꼭 릴리즈 해줘야함(C스타일이라 스마트없음)
+		*/
 		std::ostringstream oss;
 		oss << "x : " << LOWORD(lParam) << ", y : " << HIWORD(lParam) << std::endl;
 		OutputDebugStringA(oss.str().c_str());
